1167984c0705ca72cad0b5983fcac7bf8ecbc703
Brandon M <development@brandonmcginnis.com>
2017-04-26 22:02:52 -0400

    Added the response handler to the search results

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,38 +1,40 @@
 'use strict';
 
-const React = require('react');
-import { Component } from 'react'
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Image,
   View,
   TouchableHighlight,
   ListView,
-  Text
+  Text,
 } from 'react-native';
 
-// This code iterates through a list of JSON results, and parses them.
+
 class SearchResults extends Component {
 
   constructor(props) {
     super(props);
-    var dataSource = new ListView.DataSource(
-      {rowHasChanged: (r1, r2) => r1.lister_url !== r2.lister_url});
+    var dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.lister_url !== r2.lister_url});
     this.state = {
       dataSource: dataSource.cloneWithRows(this.props.listings)
     };
   }
 
-  renderRow(rowEx, sectionID, rowID) {
+  renderRow(rowData, sectionID, rowID) {
     return (
-    	{rowEx}
-  	);
+      
+        <View>
+          {rowData.title}
+        </View>
+      
+    );
   }
 
   render() {
-  	//return (
-
-  	//);
+    return (
+      <View></View>
+    );
   }
 
 }
